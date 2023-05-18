a9204afca8270ccea87f8e16f20c69461d331b63
Heron111345 <joseheronrarra@gmail.com>
2023-05-18 11:50:06 -0300

    camera em andamento

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet } from "react-native";
-import { Text, View, Button, Select } from "native-base";
+import { Text, View, Button, Select, HStack, Center } from "native-base";
+import Icon from "react-native-vector-icons/Ionicons";
 import { Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import * as Speech from "expo-speech";
@@ -13,20 +14,37 @@ export const HomeRoute = 'Home';
 
 // --> Tela Inicial
 export function Home() {
-  const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [loaded, setloaded] = useState(false);
   const cameraRef = useRef(null);
+  const [camera, setCamera] = React.useState(false);
+  const [permission, setPermission] = React.useState(false);
+  const [image, setImage] = React.useState(false);
 
-  useEffect(() => {
+  React.useEffect(() => {
     (async () => {
-      MediaLibrary.requestPermissionsAsync();
-      const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
+      try {
+        MediaLibrary.requestPermissionsAsync();
+        const cameraStatus = await Camera.requestCameraPermissionsAsync();
+        console.log(cameraStatus);
+        setPermission(true);
+
+      } catch (e) {
+        console.log(e);
+        setPermission(false);
+      }
     })();
   }, [])
 
-  if (hasCameraPermission === false) {
-    return <Text>Sem acesso a câmera!</Text>
+  const takePicture = async () => {
+    const options = {
+      quality: 0.8,
+      base64: true,
+      skipProcessing: true,
+    };
+    if (camera) {
+      const data = await camera.takePictureAsync(options);
+      setImage(data.uri);
+    }console.log(data.uri);
   }
 
   return (
@@ -44,7 +62,20 @@ export function Home() {
           ref={cameraRef}
         />
       ) : null}
+
+  <HStack position={"absolute"} py={3} w={"100%"}>
+    <HStack>
+      <Button mx={"auto"} onPress={() => takePicture()} colorScheme={"gray"} w={"56px"} height={"56px"} borderRadius={"56px"}>
+        <HStack>
+        <Icon name="camera-outline" size={28} color={"white"} />
+      </HStack>
+      </Button>
+    </HStack>
+      </HStack>
+          
+
     </View>
+    
   );
 }
 
